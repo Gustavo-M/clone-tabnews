4b06185636830ebe53e1f1da625367b40db9dafd
Gustavo Melo <5528005+Gustavo-M@users.noreply.github.com>
2023-06-27 12:07:36 +0000

    Adiciona script ´lint:check´ ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Todas as manhãs renascemos, o que fazemos hoje é o que mais importa!</h1>
+  return (
+    <h1>
+      Todas as manhãs renascemos, o que fazemos hoje é o que mais importa!
+    </h1>
+  );
 }
 
 export default Home;
